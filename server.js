f0611a2b8523f7507592892518d0ed731f0ffbcc
Jeff Romine <jromineut@gmail.com>
2017-11-18 11:02:20 -0600

    Uncomment React client.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Serve up react client
-// app.use(express.static("client/build"));
+app.use(express.static("client/build"));
 // Add routes, both API and view
 app.use(routes);
 
