66da18f196b8ab915d6c39ee323c7e5ff4948985
Jeff Romine <jromineut@gmail.com>
2017-11-24 14:57:12 -0700

    Add simple search API to server

---
+++ controllers/search-controller.js
@@ -0,0 +1,45 @@
+const elasticsearch = require('elasticsearch');
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/search.json')[env];
+
+var esClient={};
+
+if (config.use_env_variable) {
+  esClient = null; // TODO: look at how this is configured on Heroku
+} else {
+  esClient = new elasticsearch.Client(config);
+}
+
+module.exports = {
+  search: (request, response) => {
+    var body  = request.body;
+    var index = request.params.index;
+    var pageNum = 0;
+    var perPage = 5;
+    // var pageNum = request.params.page;
+    // var perPage = request.params.per_page;
+    // var userQuery = request.params.search_query;
+    // var userId = request.session.userId;
+
+    var searchParams = {
+      index: index,
+      // from: (pageNum - 1) * perPage,
+      // size: perPage,
+      body: body
+    };
+
+    esClient.search(searchParams, function (err, res) {
+      if (err) {
+        // handle error
+        throw err;
+      }
+
+      response.json({
+        results: res.hits.hits,
+        page: pageNum,
+        pages: Math.ceil(res.hits.total / perPage)
+      });
+    });
+
+  }
+}
