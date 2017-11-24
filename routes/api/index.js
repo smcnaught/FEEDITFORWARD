70af95c969ea6a56a610973d1616ee33f81d1392
Jeff Romine <jromineut@gmail.com>
2017-11-24 15:01:00 -0700

    Add service route for search

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const userRoutes = require("./user");
 const donationRoutes = require("./donation");
+const searchRoutes = require("./search");
 
 // Db routes
 router.use("/users", userRoutes);
 router.use("/donations", donationRoutes);
+router.use("/search", searchRoutes);
 
 module.exports = router;
