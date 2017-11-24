ed6504f4a612d4135b5ef9a7254d636e8bfd9816
Jeff Romine <jromineut@gmail.com>
2017-11-24 16:25:25 -0700

    Add search API call

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,6 +4,64 @@ import axios from "axios";
 
 export default {
   getDonations() {
-    return axios.get(`/api/donations`);
+    return axios.get('/api/donations');
+  },
+  searchAllDonations() {
+    return axios.post('/api/search/feed-it-forward-donations',
+      {
+        "query": {
+          "bool" : {
+            "must" : {
+              "match_all" : {}
+            },
+            "filter" : {
+              "geo_distance" : {
+                "distance" : "1000mi",
+                "location" : {
+                  "lat": 40.5680304,
+                  "lon": -111.8500344
+                }
+              }
+            }
+          }
+        },
+        "sort": [
+          {
+            "_geo_distance": {
+              "location" : {
+                "lat": 40.5680304,
+                "lon": -111.8500344
+              },
+              "order":         "asc",
+              "unit":          "mi",
+              "distance_type": "arc"
+            }
+          }
+        ]
+
+      });
   }
 };
+
+var byDistance = {
+  "query": {
+    "bool" : {
+      "must" : {
+        "match_all" : {}
+      }
+    }
+  },
+  "sort": [
+    {
+      "_geo_distance": {
+        "location" : {
+          "lat": 40.5680304,
+          "lon": -111.8500344
+        },
+        "order":         "asc",
+        "unit":          "mi",
+        "distance_type": "arc"
+      }
+    }
+  ]
+}
