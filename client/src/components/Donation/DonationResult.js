3191aa3b4f8785bc758bba76e03bd070bfd0e304
Zoe Dyksman <zbybee@hotmail.com>
2017-12-02 14:27:21 -0700

    changed pic size on donate page

--- client/src/components/Donation/DonationResult.js
+++ client/src/components/Donation/DonationResult.js
@@ -5,7 +5,7 @@ export const DonationResult = props =>
   <div>
     Product: &nbsp;{props.donation.productName}&nbsp;
     Quantity: &nbsp;{props.donation.productQuantity}&nbsp;{props.donation.productUnit}&nbsp;
-    Expiration: &nbsp;{props.donation.expiration}&nbsp;
+    {/*Expiration: &nbsp;{props.donation.expiration}&nbsp;*/}
     
   </div>;
 
